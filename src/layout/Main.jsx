04113325b1425100e90949a983061e05da3dff63
hasnainsardarr <husnainsardar07@gmail.com>
2025-02-16 00:25:07 +0500

    Created all the Home pages, using react an Daisy UI

---
+++ src/layout/Main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+ 
+const Main = () => {
+  return (
+    <div>
+         
+        <Navbar />
+        
+         
+        <Outlet />
+
+         <Footer/>
+      
+    </div>
+  )
+}
+
+export default Main
